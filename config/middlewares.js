0b634b326bcb1b6b158d82faee1ab6ec52db27d3
khoa.td <khoa.td@ssv.com.vn>
2024-08-21 12:05:26 +0700

    test trial

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,6 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
@@ -9,4 +8,36 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            '711-strapi.s3.ap-southeast-1.amazonaws.com',
+            's3.ap-southeast-1.amazonaws.com',
+            'bucket.vpce-0d352959a3c11c0a4-p7a11fsj.s3.ap-southeast-1.vpce.amazonaws.com',
+            'cms-images.7-eleven.vn',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            '711-strapi.s3.ap-southeast-1.amazonaws.com',
+            's3.ap-southeast-1.amazonaws.com',
+            'bucket.vpce-0d352959a3c11c0a4-p7a11fsj.s3.ap-southeast-1.vpce.amazonaws.com',
+            'cms-images.7-eleven.vn',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
